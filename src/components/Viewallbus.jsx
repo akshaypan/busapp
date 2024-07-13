4c414907cce2a76e8a6464cfcc4b9991f85625f5
akshaypan <akshaybenjamin7@gmail.com>
2024-07-13 15:00:37 +0530

    succ

--- src/components/Viewallbus.jsx
+++ src/components/Viewallbus.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const Viewallbus = () => {
+  var [data,changeData]=useState(
+    [
+      {"Model":"Volvo GTR 7","Brand":"Volvo","price":700000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 4","Brand":"Volvo","price":100000,"image":"https://imgs.search.brave.com/Xfc_U2J0aOZ0r6bLZK4gygg-tbdkyje4qvvjA7710mo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/NTIxNzQ4L3Bob3Rv/L2J1cy5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9MGVSQVcw/U1ByNHRtN1N6M1Y3/MkV2RGtOM2VEdXJi/dmRSekNCN2VVLV9r/ST0"},
+      {"Model":"Bajaj 750","Brand":"Bajaj","price":650000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Tata 600","Brand":"Tata","price":900000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 900","Brand":"Volvo","price":300000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 900","Brand":"Volvo","price":300000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 900","Brand":"Volvo","price":300000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 900","Brand":"Volvo","price":300000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"},
+      {"Model":"Volvo GTR 900","Brand":"Volvo","price":300000,"image":"https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn"}
+    ]
+  )
   return (
     <div>
         <NavigationBar/>
@@ -9,149 +22,35 @@ const Viewallbus = () => {
       <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <div className="row g-3">
-           <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+          
+            {data.map(
+              (value,index)=>{
+                return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-           <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
+                <div class="card">
+                  <img src={value.image} class="card-img-top" alt="..."/>
+                  <div class="card-body">
+                    <h5 class="card-title">{value.Model}</h5>
+                    <p class="card-text">{value.Brand}</p>
+                    <p class="card-text">Rs.{value.price}</p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+    
+                </div>
+              }
+            )}
+            
+            
+            
+            
 
-          </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://imgs.search.brave.com/7n1YO7qkyw0R19gC5WncBbgFt_uMFW3dV4huAdYllL0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy1zd2VkZW4u/aW1hZ2V2YXVsdC5t/ZWRpYS9wdWJsaXNo/ZWRtZWRpYS8wbWpx/cm5tbGlmOXA3bTJm/bjR0Yy9Wb2x2b19C/dXNlc19jYXNlXzEu/anBn" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Volvo B7R</h5>
-                <p class="card-text">Volvo</p>
-                <p class="card-text">Rs.500000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
+           
+            
           </div>
         </div>
   
-      <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-        <table class="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">SI No.</th>
-                        <th scope="col">Bus Model</th>
-                        <th scope="col">Bus company</th>
-                        <th scope="col">Bus Price</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Volvo B7R</td>
-                        <td>Volvo</td>
-                        <td>500000</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <td>Volvo B5R</td>
-                        <td>Volvo</td>
-                        <td>80000</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td>Luxbus 70</td>
-                        <td>Luxbus</td>
-                        <td>90000</td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-        </div>
-      </div>
+      
       </div>
     </div>
 
