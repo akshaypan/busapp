f3a9e166053602618f7f16f756506174341d4cc3
akshaypan <akshaybenjamin7@gmail.com>
2024-07-15 10:26:35 +0530

    success

--- src/components/Addbus.jsx
+++ src/components/Addbus.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const Addbus = () => {
+    const [data,changeData]=useState(
+        {"model":"","company":"","price":""}
+    )
+
+
+    const inputHandler=(event)=>{
+changeData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValues=()=>{
+        console.log(data)
+        alert("bus data submitted successfuly")
+        changeData(    {"model":"","company":"","price":""})
+    }
   return (
     <div>
         <NavigationBar/>
@@ -11,18 +25,18 @@ const Addbus = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Bus model</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="model" value={data.model} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Bus company</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="company" value={data.company} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Bus price</label>
-                            <input type="number" name="" id="" className="form-control" />
+                            <input type="number"  className="form-control" name="price" value={data.price} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-success">Add</button>
+                            <button className="btn btn-success" onClick={readValues}>Add</button>
                         </div>
                     </div>
                 </div>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,7 +7,7 @@ const NavigationBar = () => {
         
         <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Bus app</a>
+    <Link class="navbar-brand" to="#">Bus app</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
